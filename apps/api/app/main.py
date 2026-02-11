14e3e3ba4ea6652c8d0c1bbecfb4252e94306b5d
yorch88 <yorch311288@gmail.com>
2026-02-10 17:20:37 -0700

    test main.py

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -14,14 +14,17 @@ from app.modules.auth.router import router as auth_router
 # App Initialization
 # ==========================
 
+# app = FastAPI(
+#     title="Borderlink API",
+#     lifespan=lifespan,
+#     docs_url=None,
+#     redoc_url=None,
+#     openapi_url=None
+# )
 app = FastAPI(
     title="Borderlink API",
-    lifespan=lifespan,
-    docs_url=None,
-    redoc_url=None,
-    openapi_url=None
+    lifespan=lifespan
 )
-
 # ==========================
 # CORS
 # ==========================
@@ -29,6 +32,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://mentalia.borderlink.mx",
         "http://localhost:5173",
         "http://localhost:3000",
         "http://20.64.254.191:5173"
@@ -58,35 +62,37 @@ def health():
 # Protected Swagger
 # ==========================
 
-security = HTTPBasic()
+# security = HTTPBasic()
 
-SWAGGER_USER = os.getenv("SWAGGER_USER", "admin")
-SWAGGER_PASSWORD = os.getenv("SWAGGER_PASSWORD", "supersecret")
+# SWAGGER_USER = os.getenv("SWAGGER_USER", "admin")
+# SWAGGER_PASSWORD = os.getenv("SWAGGER_PASSWORD", "supersecret")
 
-def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, SWAGGER_USER)
-    correct_password = secrets.compare_digest(credentials.password, SWAGGER_PASSWORD)
+# def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
+#     correct_username = secrets.compare_digest(credentials.username, SWAGGER_USER)
+#     correct_password = secrets.compare_digest(credentials.password, SWAGGER_PASSWORD)
 
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Unauthorized",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-
-
-@app.get("/docs", include_in_schema=False)
-def protected_swagger(credentials: HTTPBasicCredentials = Depends(verify_credentials)):
-    return get_swagger_ui_html(
-        openapi_url="/openapi.json",
-        title="Borderlink API - Docs"
-    )
+#     if not (correct_username and correct_password):
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Unauthorized",
+#             headers={"WWW-Authenticate": "Basic"},
+#         )
 
 
-@app.get("/openapi.json", include_in_schema=False)
-def openapi(credentials: HTTPBasicCredentials = Depends(verify_credentials)):
-    return get_openapi(
-        title=app.title,
-        version="1.0.0",
-        routes=app.routes,
-    )
+# @app.get("/docs", include_in_schema=False)
+# def protected_swagger(credentials: HTTPBasicCredentials = Depends(verify_credentials)):
+#     return get_swagger_ui_html(
+#         openapi_url="/api/openapi.json",
+#         title="Borderlink API - Docs"
+#     )
+
+
+# @app.get("/openapi.json", include_in_schema=False)
+# def openapi():
+#     return get_openapi(
+#         title=app.title,
+#         version="1.0.0",
+#         routes=app.routes,
+#     )
+
+
